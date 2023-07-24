2cd16315663797d3d46e0e3404f3e337de2977a6
Leonardo Alves <113270070+lleonardooalves@users.noreply.github.com>
2023-07-24 14:16:34 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Outro arquivo TypeScript
-const data_1 = require("./data");
-console.log('Leonardo');
-console.log(data_1.posts);
-console.log(data_1.comments);
